983684561f8a83491e60f3dbfe0c7c3952295efb
Filipe Bezerra <filipebzerra@gmail.com>
2020-09-27 14:21:56 -0300

    Implement persistence layer with Room and test it

--- app/src/androidTest/java/com/example/android/trackmysleepquality/SleepDatabaseTest.kt
+++ app/src/androidTest/java/com/example/android/trackmysleepquality/SleepDatabaseTest.kt
@@ -16,57 +16,156 @@
 
 package com.example.android.trackmysleepquality
 
-//import androidx.room.Room
-//import androidx.test.ext.junit.runners.AndroidJUnit4
-//import androidx.test.platform.app.InstrumentationRegistry
-//import com.example.android.trackmysleepquality.database.SleepDatabase
-//import com.example.android.trackmysleepquality.database.SleepDatabaseDao
-//import com.example.android.trackmysleepquality.database.SleepNight
-//import org.junit.Assert.assertEquals
-//import org.junit.After
-//import org.junit.Before
-//import org.junit.Test
-//import org.junit.runner.RunWith
-//import java.io.IOException
-//
-///**
-// * This is not meant to be a full set of tests. For simplicity, most of your samples do not
-// * include tests. However, when building the Room, it is helpful to make sure it works before
-// * adding the UI.
-// */
-//
-//@RunWith(AndroidJUnit4::class)
-//class SleepDatabaseTest {
-//
-//    private lateinit var sleepDao: SleepDatabaseDao
-//    private lateinit var db: SleepDatabase
-//
-//    @Before
-//    fun createDb() {
-//        val context = InstrumentationRegistry.getInstrumentation().targetContext
-//        // Using an in-memory database because the information stored here disappears when the
-//        // process is killed.
-//        db = Room.inMemoryDatabaseBuilder(context, SleepDatabase::class.java)
-//                // Allowing main thread queries, just for testing.
-//                .allowMainThreadQueries()
-//                .build()
-//        sleepDao = db.sleepDatabaseDao
-//    }
-//
-//    @After
-//    @Throws(IOException::class)
-//    fun closeDb() {
-//        db.close()
-//    }
-//
-//    @Test
-//    @Throws(Exception::class)
-//    fun insertAndGetNight() {
-//        val night = SleepNight()
-//        sleepDao.insert(night)
-//        val tonight = sleepDao.getTonight()
-//        assertEquals(tonight?.sleepQuality, -1)
-//    }
-//}
-//
-//
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.example.android.trackmysleepquality.database.SleepDatabase
+import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+import com.example.android.trackmysleepquality.database.SleepNight
+import com.example.android.trackmysleepquality.lifecycle.util.observeOnce
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+import java.util.concurrent.TimeUnit
+
+
+@RunWith(AndroidJUnit4::class)
+class SleepDatabaseTest {
+    private lateinit var db: SleepDatabase
+    private lateinit var sleepDao: SleepDatabaseDao
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun createDb() {
+        val context = InstrumentationRegistry.getInstrumentation().context
+        db = Room.inMemoryDatabaseBuilder(context, SleepDatabase::class.java)
+                .allowMainThreadQueries()
+                .build()
+        sleepDao = db.sleepDatabaseDao
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        db.close()
+    }
+
+    @Test
+    @Throws(IOException::class)
+    fun given_correct_sleep_night_when_adding_to_db_then_insert_sleep_night() {
+        val sleepNight = SleepNight(
+                startTimeMilli = System.currentTimeMillis(),
+                endTimeMilli = System.currentTimeMillis().plus(
+                        TimeUnit.HOURS.convert(4L, TimeUnit.MILLISECONDS)),
+                sleepQuality = 1
+        )
+        sleepDao.insert(sleepNight)
+        val tonight = sleepDao.getTonight()
+        assertNotNull(tonight?.nightId)
+        assertEquals(sleepNight.startTimeMilli, tonight?.startTimeMilli)
+        assertEquals(sleepNight.endTimeMilli, tonight?.endTimeMilli)
+        assertEquals(1, tonight?.sleepQuality)
+    }
+
+    @Test
+    @Throws(IOException::class)
+    fun given_correct_sleep_night_when_updating_to_db_then_update_sleep_night() {
+        val sleepNight = SleepNight()
+        sleepDao.insert(sleepNight)
+        val tonight = sleepDao.getTonight()
+        tonight!!.sleepQuality = 2
+        sleepDao.update(tonight)
+        assertEquals(2, sleepDao.getTonight()?.sleepQuality)
+    }
+
+    @Test
+    @Throws(IOException::class)
+    fun given_existing_night_id_when_getting_from_db_then_get_sleep_night() {
+        val sleepNight = SleepNight()
+        sleepDao.insert(sleepNight)
+        val fromDb = sleepDao.get(1)
+        assertNotNull(fromDb)
+        assertEquals(-1, fromDb!!.sleepQuality)
+    }
+
+    @Test
+    @Throws(IOException::class)
+    fun given_various_sleep_nights_when_inserting_and_cleaning_then_sleep_night_table_is_empty() {
+        val firstSleepNight = SleepNight(
+                startTimeMilli = System.currentTimeMillis().minus(
+                        TimeUnit.DAYS.convert(2, TimeUnit.MILLISECONDS)),
+                sleepQuality = 1
+        )
+        val secondSleepNight = SleepNight(
+                startTimeMilli = System.currentTimeMillis().minus(
+                        TimeUnit.DAYS.convert(1, TimeUnit.MILLISECONDS)),
+                sleepQuality = 2
+        )
+        val thirdSleepNight = SleepNight(
+                sleepQuality = 3
+        )
+        sleepDao.insert(firstSleepNight)
+        sleepDao.insert(secondSleepNight)
+        sleepDao.insert(thirdSleepNight)
+        sleepDao.clear()
+
+        sleepDao.getAllSleepNights().observeOnce {
+            assertEquals(0, it.size)
+        }
+    }
+
+    @Test
+    @Throws(IOException::class)
+    fun given_various_sleep_nights_when_inserting_then_get_all_sleep_nights() {
+        val firstSleepNight = SleepNight(
+                startTimeMilli = System.currentTimeMillis().minus(
+                        TimeUnit.DAYS.convert(2, TimeUnit.MILLISECONDS)),
+                endTimeMilli = System.currentTimeMillis().plus(
+                        TimeUnit.HOURS.convert(2L, TimeUnit.MILLISECONDS)),
+                sleepQuality = 1
+        )
+        val secondSleepNight = SleepNight(
+                startTimeMilli = System.currentTimeMillis().minus(
+                        TimeUnit.DAYS.convert(1, TimeUnit.MILLISECONDS)),
+                endTimeMilli = System.currentTimeMillis().plus(
+                        TimeUnit.HOURS.convert(3L, TimeUnit.MILLISECONDS)),
+                sleepQuality = 2
+        )
+        val thirdSleepNight = SleepNight(
+                endTimeMilli = System.currentTimeMillis().plus(
+                        TimeUnit.HOURS.convert(4L, TimeUnit.MILLISECONDS)),
+                sleepQuality = 3
+        )
+        sleepDao.insert(firstSleepNight)
+        sleepDao.insert(secondSleepNight)
+        sleepDao.insert(thirdSleepNight)
+
+        sleepDao.getAllSleepNights().observeOnce {
+            assertEquals(3, it.size)
+        }
+    }
+
+    @Test
+    @Throws(IOException::class)
+    fun given_correct_sleep_night_when_adding_to_db_then_get_tonight() {
+        val sleepNight = SleepNight(
+                startTimeMilli = System.currentTimeMillis(),
+                endTimeMilli = System.currentTimeMillis().plus(
+                        TimeUnit.MINUTES.convert(30L, TimeUnit.MILLISECONDS))
+        )
+        sleepDao.insert(sleepNight)
+        val tonight = sleepDao.getTonight()
+        assertNotNull(tonight)
+        assertEquals(sleepNight.startTimeMilli, tonight?.startTimeMilli)
+        assertEquals(sleepNight.endTimeMilli, tonight?.endTimeMilli)
+        assertEquals(-1, tonight!!.sleepQuality)
+    }
+}
--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -16,4 +16,31 @@
 
 package com.example.android.trackmysleepquality.database
 
-interface SleepDatabaseDao
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+
+@Dao
+interface SleepDatabaseDao {
+
+    @Insert
+    fun insert(sleepNight: SleepNight)
+
+    @Update
+    fun update(sleepNight: SleepNight)
+
+    @Query("SELECT * FROM daily_sleep_quality_table WHERE night_id = :key")
+    fun get(key: Long): SleepNight?
+
+    @Query("DELETE FROM daily_sleep_quality_table")
+    fun clear()
+
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY night_id DESC")
+    fun getAllSleepNights(): LiveData<List<SleepNight>>
+
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY night_id LIMIT 1")
+    fun getTonight(): SleepNight?
+}
